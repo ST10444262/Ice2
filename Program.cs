d97a59cbac20086562e573e28448ca5a9a25558b
lab_services_student <lab_services_student@lab7L95SR>
2025-03-06 08:50:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create class object
+            WorkerClass myWorker = new WorkerClass();
+
+            //Play audio
+            myWorker.PlayAudio();
+
+        }
+    }
+}
+//--------------------------END OF FILE------------------------------///
---
+++ WorkerClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Media;
+
+namespace Ice2
+{
+    class WorkerClass
+    {
+        public WorkerClass()
+        {
+
+        }
+        public void PlayAudio()
+        {
+            //.wav file locations
+            String wav1 = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "eastwood_lawyers.wav");
+            String wav2 = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "edison.wav");
+            String wav3 = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "graham_any_nation.wav");
+
+            //Create object for each sound
+            SoundPlayer player1 = new SoundPlayer(wav1);
+            SoundPlayer player2 = new SoundPlayer(wav2);
+            SoundPlayer player3 = new SoundPlayer(wav3);
+
+            //Load and play each sound
+            player1.PlaySync();
+            player1.PlaySync();
+            player3.PlaySync();
+
+
+        }
+      
+
+
+    }
+}
+//--------------------------END OF FILE------------------------------///
